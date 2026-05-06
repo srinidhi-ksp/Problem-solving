c60e5586b48fdea65911e5ac8c2122c55f32948a
Srinidhi K <srinidhieshp050_bcs28@mepcoeng.ac.in>
2026-05-07 01:50:49 +0530

    Create TheReport.sql

---
+++ DataBaseManagementSystem/HackerRank/TheReport.sql
@@ -0,0 +1,20 @@
+SELECT c.company_code,
+SELECT 
+    CASE 
+        WHEN g.grade < 8 THEN 'NULL'
+        ELSE s.name
+    END AS name,
+    g.grade,
+    s.marks
+FROM Students s
+JOIN Grades g
+ON s.marks BETWEEN g.min_mark AND g.max_mark
+ORDER BY 
+    g.grade DESC,
+    CASE 
+        WHEN g.grade >= 8 THEN s.name
+    END ASC,
+    CASE 
+        WHEN g.grade < 8 THEN s.marks
+    END ASC;
+exit;
