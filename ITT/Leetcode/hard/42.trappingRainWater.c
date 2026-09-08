69fe596af411457488075eb7aba77e5c1e6a2dda
Srinidhi K <srinidhieshp050_bcs28@mepcoeng.ac.in>
2026-09-08 14:39:58 +0530

    Create 42.trappingRainWater.c

---
+++ ITT/Leetcode/hard/42.trappingRainWater.c
@@ -0,0 +1,34 @@
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int trappedWater = 0;
+        
+        while (left < right) {
+            // Process the side with the lower boundary bottleneck
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        
+        return trappedWater;
+    }
+}
